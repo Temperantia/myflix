45061f43cba3c9c5a9280ab3e83c1690a8232787
adipaolo54 <anthony.m.dipaolo@gmail.com>
2021-01-25 11:14:08 -0500

    css fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 v-app.app(app)
   v-container.pa-0.h-100(fluid)
-    v-row.pa-0.h-100
-      v-col.h-100(cols='12', md='7')
+    v-row.pa-0.h-100.ma-0
+      v-col.h-100.pr-0(cols='12', md='7')
         v-container.h-100.d-flex.flex-column.justify-space-between(fluid)
           v-row
             v-col(cols='4', md='2')
@@ -26,30 +26,11 @@ v-app.app(app)
                   .font-weight-bold.black--text SUBSCRIBE
           v-row(align='end')
             v-col
-              p.white-font--text MyFlix is not endorsed, moderated, owned by or affiliated with Netflix or any of its partners in any capacity. The authors of this site also have no affiliation with Netflix. MyFlix is a unofficial fansite for Netflix. All promotional material including but not limited to trailers, images and videos are all copyright to their respective owners. Netflix is a registered trademark of Netflix, Inc.
-      v-col.h-100(cols='12', md='5')
-        v-container.h-100(fluid)
-          v-row(:class='$vuetify.breakpoint.mdAndUp ? "h-33" : ""')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 37.png')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 30.png')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 33.png')
-          v-row(:class='$vuetify.breakpoint.mdAndUp ? "h-33" : ""')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 32.png')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 36.png')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 31.png')
-          v-row(:class='$vuetify.breakpoint.mdAndUp ? "h-33" : ""')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 35.png')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 34.png')
-            v-col.pa-0(cols='12', md='4')
-              img(src='/Layer 38.png')
+              p.legal.pr-0.mb-0 MyFlix is not endorsed, moderated, owned by or affiliated with Netflix or any of its partners in any capacity. The authors of this site also have no affiliation with Netflix. MyFlix is a unofficial fansite for Netflix. All promotional material including but not limited to trailers, images and videos are all copyright to their respective owners. Netflix is a registered trademark of Netflix, Inc.
+      v-col.h-100.pa-0(cols='12', md='5')
+        v-container.h-100.pa-0(fluid)
+          v-col.pa-0.boxart(cols='12')
+            
 </template>
 <script>
 export default {
@@ -78,6 +59,19 @@ export default {
 
   p {
     font-size: 8px;
+  }
+
+  .boxart {
+    display: block;
+    height: 100%;
+    width: 100%;
+    background-image: url('/splashBoxArt.png');
+    background-size: cover;
+  }
+
+  .legal {
+    font-size: 12px;
+    color: rgba(255,255,255,0.2)
   }
 }
 </style>
