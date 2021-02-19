70b05d96b82449992d299a220834e802ed1fe898
Temperance <adulorier@gmail.com>
2021-02-19 22:18:43 +0100

    test api

--- api/date.go
+++ api/date.go
@@ -3,8 +3,10 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	currentTime := time.Now().Format(time.RFC850)
+	fmt.Fprintf(w, currentTime)
 }
