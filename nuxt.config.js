bea29e7687ff4d60d909e123b8c07a9230a7f4c3
Temperance <adulorier@gmail.com>
2021-02-25 13:42:08 +0100

    remove backend logic

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,6 @@
 export default {
   target: "server",
-  dev: true,
-  build: {
-    extend(config, { isServer }) {
-      if (isServer) {
-        config.externals = {
-          "@firebase/app": "commonjs @firebase/app",
-          "@firebase/auth": "commonjs @firebase/auth",
-          "@firebase/firestore": "commonjs @firebase/firestore"
-        };
-      }
-    }
-  },
-  plugins: [
-    "~/plugins/firebase",
-    "~/plugins/auth",
-    "~/plugins/vue-scrollactive",
-    "~/plugins/ad.client"
-  ],
   components: true,
-  buildModules: [],
   modules: [
     "@nuxtjs/moment",
     "@nuxtjs/vuetify",
@@ -29,46 +10,12 @@ export default {
     "@nuxtjs/imagemin",
     "@nuxtjs/pwa",
 
-    "@nuxtjs/firebase",
     "nuxt-vuex-localstorage",
     "@nuxtjs/google-adsense",
     ["@nuxtjs/toast", { duration: 5000, position: "bottom-center" }],
-    "@nuxtjs/robots",
     "cookie-universal-nuxt",
     "vue-social-sharing/nuxt"
   ],
-  firebase: {
-    /* config: {
-      apiKey: "AIzaSyCUYPpn9MwXRg5bTohGcJEfeC5A9WMSEGc",
-      authDomain: "myflix-prod.firebaseapp.com",
-      databaseURL: "https://myflix-prod.firebaseio.com/",
-      projectId: "myflix-prod",
-      storageBucket: "myflix-prod.appspot.com",
-      messagingSenderId: "512304943565",
-      appId: "1:512304943565:web:2e0507e8a0dc807fc8f484",
-      measurementId: "G-S0Y1CVJVWP"
-    }, */
-    config: {
-      apiKey: "AIzaSyAZ4mK0FX2Iaj656xIGONrv0iTISDreNY8",
-      authDomain: "my-flix-91e46.firebaseapp.com",
-      databaseURL: "https://my-flix-91e46.firebaseio.com/",
-      projectId: "my-flix-91e46",
-      storageBucket: "my-flix-91e46.appspot.com",
-      messagingSenderId: "586880609683",
-      appId: "1:586880609683:web:0cbcb01a1db9121152cc66",
-      measurementId: "G-QG7LKFWBJR"
-    },
-    services: {
-      firestore: {
-        enablePersistence: true
-      },
-      auth: {
-        initialize: {
-          onAuthStateChangedMutation: "ON_AUTH_STATE_CHANGED_MUTATION"
-        }
-      }
-    }
-  },
   css: ["@/assets/main.scss", "@/node_modules/swiper/swiper-bundle.css"],
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
